335f19c8cc85a97fd2d8163d8107b2af92397639
antocisale <antonella.cisale@gmail.com>
2020-03-06 12:23:19 -0300

    agregadas funciones y estilos

--- src/Sections/Links.js
+++ src/Sections/Links.js
@@ -1,47 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faHome} from '@fortawesome/free-solid-svg-icons';
 import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
 import '../App.scss';
 import useSwitchBoolean from '../Hooks/SwitchBoolean';
 
 const Links = ()=>{
-    const [menu,toggleMenu] = useSwitchBoolean(false);
+    const [menu,toggleMenu] = useSwitchBoolean();
+    
 
-    const showMenu = ()=>{
+    const showMenu = e=>{
         toggleMenu();
-
     };
+
+    const unwrapMenu = menu ? "nav wrapper shown" : "nav wrapper";
 
     return(
         <div className="nav">
             <button onClick={showMenu}>
-                <FontAwesomeIcon icon={faBars}/>
+                {
+                    menu ? <FontAwesomeIcon icon={faTimes}/> : <FontAwesomeIcon icon={faBars}/>
+                }
+                
             </button>
-            <nav className="nav wrapper">
+            <nav className={unwrapMenu}>
                 <Link to="/">
                     <FontAwesomeIcon icon={faHome}/>
                 </Link>
-                <Link to="/lunes">
+                <Link to="/lunes" onClick={showMenu}>
                     Lunes
                 </Link>
-                <Link to="/martes">
+                <Link to="/martes" onClick={showMenu}>
                     Martes
                 </Link>
-                <Link to="/miercoles">
+                <Link to="/miercoles" onClick={showMenu}> 
                     Miércoles
                 </Link>
-                <Link to="/jueves">
+                <Link to="/jueves" onClick={showMenu}>
                     Jueves
                 </Link>
-                <Link to="/viernes">
+                <Link to="/viernes" onClick={showMenu}>
                     Viernes
                 </Link>
-                <Link to="/sabado">
+                <Link to="/sabado" onClick={showMenu}>
                     Sábado
                 </Link>
-                <Link to="/domingo">
+                <Link to="/domingo" onClick={showMenu}>
                     Domingo
                 </Link>
             </nav>
